60e6e6a7d3bb3f71bdd12330f11fd57ee45c672b
Shahzad Shahzada <shahzada.s@outlook.com>
2025-06-21 22:13:28 -0500

    Initial Commit

---
+++ WhatIsFootball/WhatIsFootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatIsFootballApp.swift
+//  WhatIsFootball
+//
+//  Created by Shahzad Shahzada on 2025-06-21.
+//
+
+import SwiftUI
+
+@main
+struct WhatIsFootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
